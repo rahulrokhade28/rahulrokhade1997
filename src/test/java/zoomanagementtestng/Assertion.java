64f5fa58c19bc987cc504695d00dc3aca36c25ae
Admin <Admin@DESKTOP-C0O15PC>
2022-09-21 18:54:57 +0530

    MASTER PUSH

---
+++ src/test/java/zoomanagementtestng/Assertion.java
@@ -0,0 +1,18 @@
+package zoomanagementtestng;
+
+import org.testng.annotations.Test;
+
+public class Assertion {
+
+	@Test
+	//gdg1
+		void test() {
+
+		//commit
+
+		System.out.println("hey");
+		System.out.println("tqq");
+		System.out.println("hello");
+		System.out.println("hi");
+	}
+}
